0f5abb5d512bd5216e69cb67ca542c24f1945ac9
Claudio Ippolito <ippolitop@gmail.com>
2019-01-13 14:48:43 +0000

    Refactoring: general print function

--- news.py
+++ news.py
@@ -25,18 +25,19 @@ def db_connection(query):
   c.execute(query)
   return c.fetchall()
   db.close()
+
+def print_query(answers, addition):
+  for answer in answers:
+        print('\t' + str(answer[0]) + ' - ' + str(answer[1]) + ' ' + addition)
   
 def question(query):
   answers = db_connection(query)
   if query == query_one:
     print('Q1: What are the most popular three articles of all time?')
-    for answer in answers:
-        print('\t' + str(answer[0]) + ' - ' + str(answer[1]) + 'views')
-
+    print_query(answers, 'views')
   if query == query_two:
     print('Q2: Who are the most popular article authors of all time?')
-    for answer in answers:
-        print('\t' + str(answer[0]) + ' - ' + str(answer[1]) + 'views')
+    print_query(answers, 'views')
 
 if __name__ == '__main__':
   question(query_one)
